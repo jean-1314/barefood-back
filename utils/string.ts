4b6762b4887f384fdb88841488bf5a179707969c
Ivan Savitsky <ivan@savitsky.biz>
2020-10-13 14:31:23 +0300

    Update latinize logic

--- utils/string.ts
+++ utils/string.ts
@@ -12,22 +12,10 @@ export const latinize = (str: string): string => {
     newStr += ch in c ? c[ch] : ch;
   }
   return newStr;
-  // const newString = str.toString();
-  // const from = [
-  //   'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф',
-  //   'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'];
-  // const to = [
-  //   'a', 'b', 'v', 'g', 'd', 'e', 'yo', 'zh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f',
-  //   'h', 'ts', 'tsch', 'sh', 'shch', 'y', 'y', '\'', 'e', 'yu', 'ya'];
-  // for (const key in from) {
-  //   str = newString.replace(new RegExp(from[key], 'g'), to[key]);
-  // }
-  // return str.toLowerCase();
 };
 
 export const slugify = (str: string): string => {
   const latinString = latinize(str);
-  console.log(latinString);
   return latinString
     .normalize('NFD')
     .trim()
